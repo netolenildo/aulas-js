553be7380d6ce06e9f29cf66bc3caca9a77b9841
netolenildo <32149632+netolenildo@users.noreply.github.com>
2020-02-08 17:54:44 -0300

    Add files via upload

---
+++ exercicios-js/fundamentos/booleanos.js
@@ -0,0 +1,31 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('pra finalizar...')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = 'Lenildo'
+console.log(nome || 'Desconhecido') //Caso o nome seja vazio, a string 'Desconhecido será impressa no console. Ou seja imprime o primeiro valor verdadeiro'
---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,5 @@
+ console.log("Senteça de código")
+
+ {
+    {}
+ }
